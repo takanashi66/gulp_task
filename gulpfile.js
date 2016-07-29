850aab53e74e211296f5eb18fa2b40a489df2df1
takanashi66 <takanashi66@gmail.com>
2016-07-29 23:39:08 +0900

    修正

--- gulpfile.js
+++ gulpfile.js
@@ -7,51 +7,50 @@ gulp.task('default', ['browser-sync']);
 
 //通常
 gulp.task('browser-sync', function() {
-	browserSync({
-		server: {
-			baseDir: "../" //監視するディレクトリ
-			,index  : "index.html"
-		}
-	});
+    browserSync({
+        server: {
+             baseDir: '../' //監視するディレクトリ
+            ,index  : 'index.html'
+        }
+    });
 });
 
 
 gulp.task('compass', function() {
-	gulp.src('*.scss')
-	.pipe(plumber({
-		errorHandler: function (error) {
-		console.log(error.message);
-		this.emit('end');
-	}}))
-	.pipe(compass({
-		css: '../common/css',
-		sass: '../common/sass',
-	}))
+  gulp.src('*.scss')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(compass({
+      css: '../common/css',
+      sass: '../common/sass',
+    }))
+    .pipe(gulp.dest('../common/css'));
 });
 
 
 //ブラウザリロード
 gulp.task('bs-reload', function () {
-	browserSync.reload();
+    browserSync.reload();
 });
 
 
 gulp.task('watch', function(){
-	gulp.watch('*.scss', function(event) {
-		gulp.run('compass');
-	});
+    gulp.watch('../common/sass/*.scss', function(event) {
+        gulp.run('compass');
+    });
 });
 
 
 
 //監視するファイル
 gulp.task('default', ['browser-sync'], function () {
-	gulp.watch("../*.html",									['bs-reload']);
-	gulp.watch("../**/*.html",							['bs-reload']);
-	gulp.watch("../**/*.php",								['bs-reload']);
-	gulp.watch("../*.php",									['bs-reload']);
-	gulp.watch("../common/css/*.css",				['bs-reload']);
-	gulp.watch("../common/sass/*.scss",			['bs-reload']);
-	gulp.watch("../common/js/*.js",					['bs-reload']);
-	gulp.watch('../common/sass/**/*.scss',	['compass']);
+    gulp.watch('../*.html',            ['bs-reload']);
+    gulp.watch('../*.php',            ['bs-reload']);
+    gulp.watch('../common/css/*.css', ['bs-reload']);
+    gulp.watch('../common/sass/*.scss', ['bs-reload']);
+    gulp.watch('../common/js/*.js',   ['bs-reload']);
+    gulp.watch('../common/sass/**/*.scss', ['compass']);
 });
